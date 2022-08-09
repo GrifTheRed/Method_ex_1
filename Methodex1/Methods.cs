1f23fa17c210c0cd2e764adc71739e735ed6e0bb
Derek Griffin <griffinderek6@gmail.com>
2022-08-08 19:29:32 -0600

    added methods

---
+++ Methodex1/Methods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Methodex1
+{
+    public class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        internal static int Multiply(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Methodex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Methodex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add Method
+            int answer = Methods.Add(23, 45);
+            Console.WriteLine(answer);
+
+            //Multiply Method
+            int sum = Methods.Multiply(8 * 5);
+            Console.WriteLine(answer);
+
+        }
+           
+           
+        }
+        
+    }
+
+
